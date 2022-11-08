5554ce158050d0d9bb983cc43bb591ac9156ea61
Дмитрий Гетун <dmgetun@gmail.com>
2022-11-08 14:48:08 +0300

    labHello

--- labHello.py
+++ labHello.py
@@ -1,4 +1,21 @@
+
+#комментарий
+
 print('Привет!')
 
 name = 'Маша'
-print('Привет')
+print('Привет, ' + name + '!')
+print('Привет, ',name,'!')
+print(f'Привет {name}')
+
+print(1,2,3,4)
+print(1,2,3,4,sep=', ')
+print(1,2,3,4,sep='\n')
+print(1,2,3,4,sep='_')
+
+def AAA():
+    a = 3
+    b = 2
+    print(a + b)
+    
+AAA()
