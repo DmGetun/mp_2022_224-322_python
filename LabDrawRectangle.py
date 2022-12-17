e010931b6d45100c74d46afa5e9adc308bb497c3
Дмитрий Гетун <dmgetun@gmail.com>
2022-12-17 17:55:14 +0300

    labDrawRectangle

--- LabDrawRectangle.py
+++ LabDrawRectangle.py
@@ -1,14 +1,21 @@
 symbol = '█'
-height = int(input('Введите высоту прямоугольника: '))
-width = int(input('Введите ширину прямоугольника: '))
-fill = input('Закрасить? [y/n]: ')
-isFill = fill.lower() == 'y'
+flag = True
 
-if isFill:
-    for h in range(height):
+def draw(width,height,isFill):
+    if isFill:
+        for h in range(height):
+            print(symbol * width)
+    else:
         print(symbol * width)
-else:
-    print(symbol * width)
-    for i in range(height - 2):
-        print(symbol,' ' * (width - 2), symbol,sep='')
-    print(symbol * width)
+        for i in range(height - 2):
+            print(symbol,' ' * (width - 2), symbol,sep='')
+        print(symbol * width)
+
+
+while flag:
+    height = int(input('Введите высоту прямоугольника: '))
+    width = int(input('Введите ширину прямоугольника: '))
+    fill = input('Закрасить? [y/n]: ')
+    isFill = fill.lower() == 'y'
+    draw(width, height, isFill)
+    flag = True if input('Повторить? [y/n]: ') == 'y' else False
